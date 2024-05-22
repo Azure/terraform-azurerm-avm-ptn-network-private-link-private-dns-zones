e35e787e193c1ae7c0b1768b9afba449a27178bb
Jack Tracey <jack@jacktracey.co.uk>
2024-05-22 14:11:16 +0100

    dns zone logic working

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,14 @@
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
-output "resource" {
-  description = "This is the full output for the resource."
-  value       = azurerm_resource_group.TODO # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+# output "resource" {
+#   description = "This is the full output for the resource."
+#   value       = azurerm_resource_group.TODO # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
+# }
+
+# output "out_private_link_private_dns_zones_replaced_regionName" {
+#   value = local.private_link_private_dns_zones_replaced_regionName
+# }
+
+output "out_private_link_private_dns_zones" {
+  value = local.private_link_private_dns_zones_replaced_regionCode
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,155 @@
+variable "resoruce_group_creation_enabled" {
+  type        = bool
+  default     = true
+  description = "This variable controls whether or not the resource group should be created. If set to false, the resource group must be created elsewhere and the resource group name must be provided to the module. If set to true, the resource group will be created by the module using the name provided in `resource_group_name`."
+  nullable    = false
+}
+
 variable "resource_group_name" {
   type        = string
   description = "The resource group where the resources will be deployed."
+}
+
+variable "private_link_private_dns_zones" {
+  type = set(string)
+  default = [
+    "privatelink.api.azureml.ms",
+    "privatelink.notebooks.azure.net",
+    "privatelink.cognitiveservices.azure.com",
+    "privatelink.openai.azure.com",
+    "privatelink.directline.botframework.com",
+    "privatelink.token.botframework.com",
+    "privatelink.servicebus.windows.net",
+    "privatelink.servicebus.windows.net",
+    "privatelink.datafactory.azure.net",
+    "privatelink.adf.azure.com",
+    "privatelink.azurehdinsight.net",
+    "privatelink.{regionName}.kusto.windows.net",
+    "privatelink.blob.core.windows.net",
+    "privatelink.queue.core.windows.net",
+    "privatelink.table.core.windows.net",
+    "privatelink.analysis.windows.net",
+    "privatelink.pbidedicated.windows.net",
+    "privatelink.tip1.powerquery.microsoft.com",
+    "privatelink.azuredatabricks.net",
+    "{regionName}.privatelink.batch.azure.com",
+    "{regionName}.service.privatelink.batch.azure.com",
+    "privatelink-global.wvd.microsoft.com",
+    "privatelink.wvd.microsoft.com",
+    "privatelink.wvd.microsoft.com",
+    "privatelink.{regionName}.azmk8s.io",
+    "privatelink.azurecr.io",
+    "{regionName}.data.privatelink.azurecr.io",
+    "privatelink.database.windows.net",
+    "privatelink.documents.azure.com",
+    "privatelink.mongo.cosmos.azure.com",
+    "privatelink.cassandra.cosmos.azure.com",
+    "privatelink.gremlin.cosmos.azure.com",
+    "privatelink.table.cosmos.azure.com",
+    "privatelink.analytics.cosmos.azure.com",
+    "privatelink.postgres.cosmos.azure.com",
+    "privatelink.postgres.database.azure.com",
+    "privatelink.postgres.database.azure.com",
+    "privatelink.mysql.database.azure.com",
+    "privatelink.mysql.database.azure.com",
+    "privatelink.mariadb.database.azure.com",
+    "privatelink.redis.cache.windows.net",
+    "privatelink.redisenterprise.cache.azure.net",
+    "privatelink.database.windows.net",
+    "privatelink.documents.azure.com",
+    "privatelink.mongo.cosmos.azure.com",
+    "privatelink.cassandra.cosmos.azure.com",
+    "privatelink.gremlin.cosmos.azure.com",
+    "privatelink.table.cosmos.azure.com",
+    "privatelink.analytics.cosmos.azure.com",
+    "privatelink.postgres.cosmos.azure.com",
+    "privatelink.postgres.database.azure.com",
+    "privatelink.postgres.database.azure.com",
+    "privatelink.mysql.database.azure.com",
+    "privatelink.mysql.database.azure.com",
+    "privatelink.mariadb.database.azure.com",
+    "privatelink.redis.cache.windows.net",
+    "privatelink.redisenterprise.cache.azure.net",
+    "privatelink.his.arc.azure.com",
+    "privatelink.guestconfiguration.azure.com",
+    "privatelink.dp.kubernetesconfiguration.azure.com",
+    "privatelink.servicebus.windows.net",
+    "privatelink.eventgrid.azure.net",
+    "privatelink.eventgrid.azure.net",
+    "privatelink.eventgrid.azure.net",
+    "privatelink.eventgrid.azure.net",
+    "privatelink.azure-api.net",
+    "privatelink.workspace.azurehealthcareapis.com",
+    "privatelink.fhir.azurehealthcareapis.com",
+    "privatelink.dicom.azurehealthcareapis.com",
+    "privatelink.azure-devices.net",
+    "privatelink.servicebus.windows.net1",
+    "privatelink.azure-devices-provisioning.net",
+    "privatelink.api.adu.microsoft.com",
+    "privatelink.azureiotcentral.com",
+    "privatelink.digitaltwins.azure.net",
+    "privatelink.media.azure.net",
+    "privatelink.azure-automation.net",
+    "privatelink.{regionCode}.backup.windowsazure.com",
+    "privatelink.siterecovery.windowsazure.com",
+    "privatelink.monitor.azure.com",
+    "privatelink.oms.opinsights.azure.com",
+    "privatelink.ods.opinsights.azure.com",
+    "privatelink.agentsvc.azure-automation.net",
+    "privatelink.blob.core.windows.net",
+    "privatelink.purview.azure.com",
+    "privatelink.purviewstudio.azure.com",
+    "privatelink.prod.migration.windowsazure.com",
+    "privatelink.prod.migration.windowsazure.com",
+    "privatelink.azure.com",
+    "privatelink.grafana.azure.com",
+    "privatelink.vaultcore.azure.net",
+    "privatelink.managedhsm.azure.net",
+    "privatelink.azconfig.io",
+    "privatelink.attest.azure.net",
+    "privatelink.blob.core.windows.net",
+    "privatelink.table.core.windows.net",
+    "privatelink.queue.core.windows.net",
+    "privatelink.file.core.windows.net",
+    "privatelink.web.core.windows.net",
+    "privatelink.dfs.core.windows.net",
+    "privatelink.afs.azure.net",
+    "privatelink.blob.core.windows.net",
+    "privatelink.search.windows.net",
+    "privatelink.servicebus.windows.net",
+    "privatelink.azurewebsites.net",
+    "scm.privatelink.azurewebsites.net",
+    "privatelink.service.signalr.net",
+    "privatelink.azurestaticapps.net",
+    "privatelink.servicebus.windows.net"
+  ]
+  description = <<DESCRIPTION
+A set of Private Link Private DNS Zones to create. Each element must be a valid DNS zone name.
+
+**NOTE:**
+
+- Private Link Private DNS Zones that have `{{regionCode}}` in the name will be replaced with the Geo Code of the Region you specified in the `location` variable, if available, as documented [here](https://learn.microsoft.com/azure/private-link/private-endpoint-dns#:~:text=Note-,In%20the%20above%20text%2C%20%7BregionCode%7D%20refers%20to%20the%20region%20code%20(for%20example%2C%20eus%20for%20East%20US%20and%20ne%20for%20North%20Europe).%20Refer%20to%20the%20following%20lists%20for%20regions%20codes%3A,-All%20public%20clouds).
+  - e.g. If `UK South` or `uksouth` was specified as the region in the `location` variable, `{{regionCode}}` would be replaced with `uks` in the Private DNS Zone name.
+- Private Link Private DNS Zones that have `{{regionName}}` in the name will be replaced with the short name of the Region you specified in the `location` variable, if available, as documented [here](https://learn.microsoft.com/azure/private-link/private-endpoint-dns).
+  - e.g. If `UK South` or `uksouth` was specified as the region in the `location` variable, `{{regionName}}` would be replaced with `uksouth` in the Private DNS Zone name.
+
+**IMPORTANT:**
+
+The folowing Private Link Private DNS Zones have been removed from the default value for this variable as they require additional placeholders to be replaced that will only be known by the caller of the module at runtime and cannot be determined by the module itself. If you have a requirement to create these Private Link Private DNS Zones, you must provide the full list of Private Link Private DNS Zones to create as a set in the `private_link_private_dns_zones` variable, using the default value as a reference. The list of Private Link Private DNS Zones that have been removed are:
+
+- `{subzone}.privatelink.{regionName}.azmk8s.io`
+- `privatelink.{dnsPrefix}.database.windows.net`
+- `privatelink.{partitionId}.azurestaticapps.net`
+
+DESCRIPTION
+  nullable    = false
+}
+
+variable "virtual_network_resource_ids_to_link_to" {
+  type        = set(string)
+  description = "A set of Virtual Network Resource IDs to link to the Private Link Private DNS Zones created."
+  default = []
+  nullable    = false  
 }
 
 # required AVM interfaces
@@ -22,7 +171,7 @@ variable "diagnostic_settings" {
   }))
   default     = {}
   description = <<DESCRIPTION
-A map of diagnostic settings to create on the Key Vault. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+A map of diagnostic settings to create on each of the Private Link Private DNS Zones created. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
 
 - `name` - (Optional) The name of the diagnostic setting. One will be generated if not set, however this will not be unique if you want to create multiple diagnostic setting resources.
 - `log_categories` - (Optional) A set of log categories to send to the log analytics workspace. Defaults to `[]`.
@@ -65,7 +214,7 @@ DESCRIPTION
 variable "location" {
   type        = string
   nullable    = false
-  description = "Azure region where the resource should be deployed."
+  description = "Azure region where the each of the Private Link Private DNS Zones created and Resource Group, if created, will be deployed."
 }
 
 variable "lock" {
@@ -75,7 +224,7 @@ variable "lock" {
   })
   default     = null
   description = <<DESCRIPTION
-Controls the Resource Lock configuration for this resource. The following properties can be specified:
+Controls the Resource Lock configuration for each of the Private Link Private DNS Zones created and Resource Group, if created. The following properties can be specified:
 
 - `kind` - (Required) The type of lock. Possible values are `\"CanNotDelete\"` and `\"ReadOnly\"`.
 - `name` - (Optional) The name of the lock. If not specified, a name will be generated based on the `kind` value. Changing this forces the creation of a new resource.
@@ -88,7 +237,7 @@ DESCRIPTION
 }
 
 # tflint-ignore: terraform_unused_declarations
-variable "role_assignments" {
+variable "private_link_dns_zones_role_assignments" {
   type = map(object({
     role_definition_id_or_name             = string
     principal_id                           = string
@@ -100,7 +249,33 @@ variable "role_assignments" {
   }))
   default     = {}
   description = <<DESCRIPTION
-A map of role assignments to create on this resource. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+A map of role assignments to create on each of the Private Link Private DNS Zones created. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
+
+- `role_definition_id_or_name` - The ID or name of the role definition to assign to the principal.
+- `principal_id` - The ID of the principal to assign the role to.
+- `description` - The description of the role assignment.
+- `skip_service_principal_aad_check` - If set to true, skips the Azure Active Directory check for the service principal in the tenant. Defaults to false.
+- `condition` - The condition which will be used to scope the role assignment.
+- `condition_version` - The version of the condition syntax. Valid values are '2.0'.
+
+> Note: only set `skip_service_principal_aad_check` to true if you are assigning a role to a service principal.
+DESCRIPTION
+  nullable    = false
+}
+
+variable "resource_group_role_assignments" {
+  type = map(object({
+    role_definition_id_or_name             = string
+    principal_id                           = string
+    description                            = optional(string, null)
+    skip_service_principal_aad_check       = optional(bool, false)
+    condition                              = optional(string, null)
+    condition_version                      = optional(string, null)
+    delegated_managed_identity_resource_id = optional(string, null)
+  }))
+  default     = {}
+  description = <<DESCRIPTION
+A map of role assignments to create on the Resource Group, if `resoruce_group_creation_enabled` is set to `true`. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
 
 - `role_definition_id_or_name` - The ID or name of the role definition to assign to the principal.
 - `principal_id` - The ID of the principal to assign the role to.
