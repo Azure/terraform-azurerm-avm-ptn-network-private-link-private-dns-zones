f0f6720bd350b7cfb856f42c10a6fcf0ff9dad9a
Jack Tracey <41163455+jtracey93@users.noreply.github.com>
2024-05-28 13:35:13 +0100

    changes to local calcs for zones and vnets

--- locals.tf
+++ locals.tf
@@ -124,14 +124,56 @@ locals {
     "swedensouth" : "sds"
   }
 
-  private_link_private_dns_zones_replaced_regionName = toset([for zone in var.private_link_private_dns_zones : replace(zone["zone_name"], "{regionName}", local.location_short_name)])
-  private_link_private_dns_zones_replaced_regionCode = toset([for zone in local.private_link_private_dns_zones_replaced_regionName : replace(zone, "{regionCode}", local.azure_region_geo_codes_short_name_as_key[local.location_short_name])])
+  private_link_private_dns_zones_replaced_regionName_map = { for k, v in var.private_link_private_dns_zones : k => {
+    zone_name = replace(v.zone_name, "{regionName}", local.location_short_name)
+  } }
 
-  # flattened_map_of_private_link_private_dns_zones_with_vnet_links = {
-  #   for item in flatten(
-  #     [
-  #       for zone in local.private_link_private_dns_zones_replaced_regionCode
-  #     ]
-  #   )
-  # }
+  private_link_private_dns_zones_replaced_regionCode_map = { for k, v in local.private_link_private_dns_zones_replaced_regionName_map : k => {
+    zone_name = replace(v.zone_name, "{regionCode}", local.azure_region_geo_codes_short_name_as_key[local.location_short_name])
+  } }
+
+# Use a DIY approach
+
+  combined_private_link_private_dns_zones_replaced_with_vnets_to_link = {
+    for item in flatten([
+      for zone_key, zone_value in local.private_link_private_dns_zones_replaced_regionCode_map : [
+        for vnet_key, vnet_value in var.virtual_network_resource_ids_to_link_to : {
+          zone_key   = zone_key
+          zone_value = zone_value
+          vnet_key   = vnet_key
+          vnet_value = vnet_value
+        }
+      ]
+      ]
+    ) : "${item.zone_key}/${item.vnet_key}" => item
+  }
+
+
+#########
+
+# Use setproduct()
+
+  set_of_private_link_private_dns_zones_replaced_regionCode = [
+    for k, v in local.private_link_private_dns_zones_replaced_regionCode_map : {
+      key       = k
+      zone_name = v.zone_name
+    }
+  ]
+
+  set_of_virtual_network_resource_ids_to_link_to = [
+    for k, v in var.virtual_network_resource_ids_to_link_to : {
+      key              = k
+      vnet_resource_id = v.vnet_resource_id
+    }
+  ]
+
+  product_of_private_link_private_dns_zones_replaced_combined_with_vnets_to_link = [
+    for pair in setproduct(local.set_of_private_link_private_dns_zones_replaced_regionCode, local.set_of_virtual_network_resource_ids_to_link_to) : {
+      zone_key   = pair[0].key
+      zone_value = pair[0].zone_name
+      vnet_key   = pair[1].key
+      vnet_value = pair[1].vnet_resource_id
+    }
+  ]
+
 }
