60164a2abfe08778025dc54f5c2d8bf8032603bd
Bilal Amjad <79409563+mbilalamjad@users.noreply.github.com>
2024-07-15 10:19:42 -0700

    chore: repository governance (#21)
    * fix: grept apply
    
    * add provider
    
    * pre commit
    
    * updates
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <tfmod442916@users.noreply.github.com>
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,13 @@
 terraform {
   required_version = "~> 1.5"
   required_providers {
-    # TODO: Ensure all required providers are listed here and the version property includes a constraint on the maximum major version.
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.71"
+    }
+    modtm = {
+      source  = "Azure/modtm"
+      version = "~> 0.3"
     }
     random = {
       source  = "hashicorp/random"
