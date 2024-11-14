e05f12986319c360618548d70661533174d542d7
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-11-14 13:01:34 +0000

    feat: dns zone outputs (#54)

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "combined_private_link_private_dns_zones_replaced_with_vnets_to_link" {
   value       = local.combined_private_link_private_dns_zones_replaced_with_vnets_to_link
 }
 
+output "private_dns_zone_resource_ids" {
+  description = "The map of private DNS zones to resource ids."
+  value       = { for key, mod in module.avm_res_network_privatednszone : key => mod.resource_id }
+}
+
 output "resource_group_resource_id" {
   description = "The resource ID of the resource group that the Private DNS Zones are deployed into."
   value       = local.resource_group_resource_id
